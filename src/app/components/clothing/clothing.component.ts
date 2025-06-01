108ca1280007e6e1c40ceb6deacd5492024c354e
Vincent Horecký <vincent.horecky@seznam.cz>
2025-06-01 14:45:54 +0200

    designed the date

--- src/app/components/clothing/clothing.component.ts
+++ src/app/components/clothing/clothing.component.ts
@@ -1,12 +1,12 @@
 import { Component, inject, OnInit } from "@angular/core";
-import { AsyncPipe } from "@angular/common";
+import { AsyncPipe, DatePipe } from "@angular/common";
 import { WeatherService } from "../../services/weather.service";
 import { Observable } from "rxjs";
 import type { ClothingSummary } from "../../services/weather.service";
 
 @Component({
   selector: "app-clothing",
-  imports: [AsyncPipe],
+  imports: [AsyncPipe, DatePipe],
   templateUrl: "./clothing.component.html",
   styleUrl: "./clothing.component.css",
 })
