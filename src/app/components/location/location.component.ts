e80c522ebd3b3cbcfc9818d6b0042130e4c27cf6
Vincent <vincent.horecky@seznam.cz>
2025-06-05 18:04:25 +0200

    now search for nominatim instead of btn

--- src/app/components/location/location.component.ts
+++ src/app/components/location/location.component.ts
@@ -1,20 +1,48 @@
-import { Component, inject } from "@angular/core";
+import { Component, OnInit, inject } from "@angular/core";
 import { AsyncPipe, JsonPipe } from "@angular/common";
+import { FormControl, ReactiveFormsModule } from "@angular/forms";
 import { NominatimService } from "../../services/nominatim.service";
-import { Observable } from "rxjs";
+import {
+  debounceTime,
+  distinctUntilChanged,
+  Observable,
+  switchMap,
+  of,
+  catchError,
+} from "rxjs";
 import type { Location } from "../../services/nominatim.service";
 
 @Component({
   selector: "app-location",
-  imports: [AsyncPipe, JsonPipe],
+  imports: [AsyncPipe, JsonPipe, ReactiveFormsModule],
   templateUrl: "./location.component.html",
   styleUrl: "./location.component.css",
 })
-export class LocationComponent {
+export class LocationComponent implements OnInit {
   nominatim = inject(NominatimService);
   locations$!: Observable<Location[]>;
+  search = new FormControl("");
 
   handleClick() {
     this.locations$ = this.nominatim.getLocations("Prague");
+  }
+
+  ngOnInit(): void {
+    this.locations$ = this.search.valueChanges.pipe(
+      debounceTime(1000),
+      distinctUntilChanged(),
+      switchMap((value) => {
+        if (!value || value.trim().length < 3) {
+          return of([]);
+        }
+
+        return this.nominatim.getLocations(value).pipe(
+          catchError((err) => {
+            console.error("Error while fetching locations: ", err);
+            return of([]);
+          }),
+        );
+      }),
+    );
   }
 }
